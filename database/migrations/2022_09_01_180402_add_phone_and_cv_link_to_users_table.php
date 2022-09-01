1561fc25cabb93d2407e69fd6e26eff7e8d281c6
sakhawathossensadi <newtonss919@gmail.com>
2022-09-02 00:23:43 +0600

    add phone, status and cv_link column to user tabel

---
+++ database/migrations/2022_09_01_180402_add_phone_and_cv_link_to_users_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Analyzen\Auth\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table($this->getTable(), function (Blueprint $table) {
+            $table->string('phone', 30)->nullable()->after('email_verified_at');
+            $table->tinyInteger('status')->default(1)->nullable()->comment('0=inactive, 1=active')->after('email_verified_at');
+            $table->text('cv_link')->nullable()->after('email_verified_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table($this->getTable(), function (Blueprint $table) {
+            $table->dropColumn(
+                [
+                    'phone',
+                    'cv_link',
+                ]
+            );
+        });
+    }
+
+    /**
+     * Get the table name to add and drop fields
+     *
+     * @return string
+     */
+    private function getTable(): string
+    {
+        $userModel = new User();
+
+        return $userModel->getTable();
+    }
+};
