7c9e85b8946b6b9f2dbe22cdc4b0a1896b0dfa4f
sakhawathossensadi <newtonss919@gmail.com>
2022-09-02 09:35:34 +0600

    add sample test case

---
+++ tests/Features/UserApiTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Analyzen\Auth\Tests\Features;
+
+use Analyzen\Auth\Tests\TestCase;
+
+class UserApiTest extends TestCase
+{
+    public function tests_user_registration()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->assertTrue(true);
+    }
+}
