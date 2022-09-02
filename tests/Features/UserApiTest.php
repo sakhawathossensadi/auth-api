80c09052143363dfecec817cfeb771f67abbc50a
sakhawathossensadi <newtonss919@gmail.com>
2022-09-02 14:05:21 +0600

    user registration test case

--- tests/Features/UserApiTest.php
+++ tests/Features/UserApiTest.php
@@ -2,6 +2,7 @@
 
 namespace Analyzen\Auth\Tests\Features;
 
+use Analyzen\Auth\Models\User;
 use Analyzen\Auth\Tests\TestCase;
 
 class UserApiTest extends TestCase
@@ -10,6 +11,19 @@ class UserApiTest extends TestCase
     {
         $this->withoutExceptionHandling();
 
-        $this->assertTrue(true);
+        $userData = [
+            'name' => "Sakhawat Hossen",
+            'email' => 'sakhawathossen42@gmail.com',
+            'password' => '12345678',
+            'password_confirmation' => '12345678',
+        ];
+
+        $response = $this->postJson(route('user.registration'), $userData);
+
+        $response->assertStatus(201);
+        $response->assertJsonFragment([
+            'name' => 'Sakhawat Hossen',
+            'email' => 'sakhawathossen42@gmail.com',
+        ]);
     }
 }
