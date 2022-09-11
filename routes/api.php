e7267aa9e3e5ef7f34a33eed2c807a8d7274d530
sakhawathossensadi <newtonss919@gmail.com>
2022-09-11 20:56:31 +0600

    api for logout

--- routes/api.php
+++ routes/api.php
@@ -12,5 +12,9 @@ Route::group(
         Route::group(['middleware' => 'guest:api'], function () {
             Route::post('register', [UserController::class, 'register'])->name('user.registration');
         });
+
+        Route::group(['middleware' => 'auth:api'], function () {
+            Route::any('logout', [UserController::class, 'logout'])->name('user.logout');
+        });
     }
 );
